e6c734b7c2cb1b4dfbfa7f2af87630ba1d86f064
minhyukk2 <minhyuk@buptle.com>
2023-08-29 21:18:52 +0900

    Template Method Pattern

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,82 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+
+        long startTime = System.currentTimeMillis();
+
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("resultTime={}", resultTime);
+    }
+
+
+    /**
+     * 템플릿 메서드 패턴 사용 - 변하는 것과 변하지 않는 것 분리.
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.excute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.excute();
+    }
+
+    /**
+     * 익명 내부 클래스
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.excute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template2.getClass());
+        template2.excute();
+    }
+}
